7b26e9646a22e812968f05b5b46b6fc93c842c7f
fr13n8 <ovsepanrik@gmail.com>
2021-10-26 14:04:59 +0400

    Test autodeploy

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -55,7 +55,7 @@
 
                             <div class="ml-12">
                                 <div class="mt-2 text-gray-600 dark:text-gray-400 text-sm">
-                                    Laravel has wonderful, thorough documentation covering every aspect of the framework. Whether you are new to the framework or have previous experience with Laravel, we recommend reading all of the documentation from beginning to end.
+                                    Lorem ipsum dolor sit, amet consectetur adipisicing elit. Excepturi voluptates voluptatum non! Dolor laborum, temporibus quos, doloribus fuga deleniti dignissimos sed maxime vitae, facilis dicta odio! Expedita nobis reiciendis eius.
                                 </div>
                             </div>
                         </div>
