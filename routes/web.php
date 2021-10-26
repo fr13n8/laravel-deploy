b34ca0bb4e805b80f1d1ec1bbaab9cc68cb107b4
fr13n8 <ovsepanrik@gmail.com>
2021-10-26 15:47:57 +0400

    Handle errors to slack

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/home', function () {
+    try {
+        $view = route("test");
+    } catch (\Throwable $th) {
+        throw $th;
+    }
+});
